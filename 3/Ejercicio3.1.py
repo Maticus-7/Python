218bad2412c1cc5a7806a46463c32a944476b02e
Matiu <maticusml@gmail.com>
2026-01-27 20:09:18 -0500

    A

--- 3/Ejercicio3.1.py
+++
@@ -1,9 +0,0 @@
-#¿Hay la letra 'a' en el string?XD
-string = input("Introduce un string: ")
-if 'a' in string:
-    print(f"La letra 'a' está presente en el string.")
-    for i in range(len(string)):
-        if string[i] == 'a':
-            print(f"La letra 'a' está en la posición {i}.")
-else:
-    print("La letra 'a' no está presente en el string.")
